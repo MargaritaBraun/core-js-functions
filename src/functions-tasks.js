8ebe968701d700c0c49360c6a1ac33c2c24da38c
Marharyta Parkalava <margobraun777@gmail.com>
2024-04-14 12:04:20 +0300

    feat: etIdGeneratorFunction is done

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -228,8 +228,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let id = startFrom;
+  return function abc() {
+    const newId = id;
+    id += 1;
+    return newId;
+  };
 }
 
 module.exports = {
